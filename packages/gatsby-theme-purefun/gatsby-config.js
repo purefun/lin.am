17f7d4f8b63831a0a29bc13810c0efe7421eaed3
Nil Lin <nil@lin.am>
2020-05-28 12:58:19 +0800

    feat: favicon

--- packages/gatsby-theme-purefun/gatsby-config.js
+++ packages/gatsby-theme-purefun/gatsby-config.js
@@ -4,18 +4,13 @@ module.exports = (pluginOptions) => {
       {
         resolve: 'gatsby-plugin-manifest',
         options: {
-          name: "purefun.io",
-          short_name: "purefun",
-          start_url: "/",
-          background_color: "#ffffff",
-          theme_color: "#257F6B",
-          // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-          // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-          display: "standalone",
-          icon: "src/images/favicon.png", // This path is relative to the root of the site.
-          // An optional attribute which provides support for CORS check.
-          // If you do not provide a crossOrigin option, it will skip CORS for manifest.
-          // Any invalid keyword or empty string defaults to `anonymous`
+          name: 'purefun.io',
+          short_name: 'purefun',
+          start_url: '/',
+          background_color: '#ffffff',
+          theme_color: '#257F6B',
+          display: 'standalone',
+          icon: `${__dirname}/src/static/images/favicon.png`,
           crossOrigin: `use-credentials`,
         },
       },
