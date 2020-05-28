4a6b05775b7778f48700699cd271ea4412aa9a1d
Nil Lin <nil@lin.am>
2020-05-28 13:00:28 +0800

    fix: code theme

--- packages/gatsby-theme-purefun/src/theme/MDXComponents/code-block-theme.ts
+++ packages/gatsby-theme-purefun/src/theme/MDXComponents/code-block-theme.ts
@@ -43,7 +43,7 @@ export const theme: PrismTheme = {
     {
       types: ['variable'],
       style: {
-        color: 'var(--fenced-cyan)'
+        color: 'var(--fenced-red)'
       }
     },
     {
